336605ad1e7cce026108505044151115716eab31
d3nnyyy <d3nnn41k@gmail.com>
2023-05-14 16:44:22 +0300

    adding some modifications to code

--- ua/lviv/iot/algo/part1/lab7/Chair/Chair.py
+++ ua/lviv/iot/algo/part1/lab7/Chair/Chair.py
@@ -1,12 +1,14 @@
 class Chair:
-
     _instance = None
 
-    def __init__(self, max_weight, material, owner, id=1):
-        self.max_weight = max_weight
-        self.material = material
-        self.owner = owner
-        self.id = id
+    def __init__(self):
+        pass
+
+    def __init__(self, max_weight=None, material=None, owner=None, id=1):
+        self.__max_weight = max_weight
+        self.__material = material
+        self.__owner = owner
+        self.__id = id
 
     def __str__(self):
         return "Chair: " \
@@ -19,7 +21,7 @@ class Chair:
         return self.__str__()
 
     @staticmethod
-    def getInstance(max_weight, material, owner, id):
+    def get_instance(max_weight=None, material=None, owner=None, id=1):
         if Chair._instance is None:
             Chair._instance = Chair(max_weight, material, owner, id)
         return Chair._instance
@@ -64,17 +66,5 @@ class Chair:
         self.owner = None
         f"Chair " + str(self.id) + " is released"
 
-    def isOccupied(self):
+    def is_occupied(self):
         return self.owner is not None
-
-
-chairs = []
-
-chair1 = Chair.getInstance(100, "wood", "Vasya", 1)
-chair2 = Chair.getInstance(150, "plastic", "Yurko", 2)
-
-chairs.append(chair1)
-chairs.append(chair2)
-
-print(chairs)
-print(chair1.occupy("Zenyk"))
